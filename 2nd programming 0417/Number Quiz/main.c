2723570538b677fbae8bcce60f95c0e14c480951
rlarjsdn2313 <informationdoor04@gmail.com>
2021-04-17 13:45:37 +0900

    Change Number Quiz

--- 2nd programming 0417/Number Quiz/main.c
+++ 2nd programming 0417/Number Quiz/main.c
@@ -3,19 +3,23 @@
 #include <stdlib.h>
 
 int generate_answer(int scope);
+void get_input(int* a, char* question);
 
 int main() {
     int scope = 0;
-    printf("범위를 입력하세요: ");
-    scanf("%d", &scope);
-
+    get_input(&scope, "범위를 입력하세요: ");
+    printf("\n");
+    
     int num = generate_answer(scope);
     int a = 0;
 
     while (a != num) {
-        printf("수를 입력하세요: ");
-        scanf("%d", &a);
+        get_input(&a, "숫자를 입력하세요: ");
 
+        if (a == 0) {
+            printf("정답은 %d입니다\n", num);
+            return 0;
+        }
         if (a > num) {
             printf("다운!\n");
         } else if (a < num) {
@@ -28,6 +32,11 @@ int main() {
     printf("정답!!\n");
 
     return 0;
+}
+
+void get_input(int* a, char* question) {
+    printf("%s", question);
+    scanf("%d", a);
 }
 
 int generate_answer(int scope) {
