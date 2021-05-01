08796a7c10c7cbc42960d73dfd4a746ad115225c
rlarjsdn2313 <informationdoor04@gmail.com>
2021-05-01 13:42:09 +0900

    Practice done

---
+++ 3rd probramming 0501/practice/TwoDArray-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define Col 3
+#define Row 5
+
+int main() {
+    int S[Col][Row] = {
+        {0, 1, 2, 3, 4},
+        {10, 11, 12 ,13 ,14},
+        {20, 21,22, 23, 24}
+    };
+
+    int i, j;
+    for (i = 0; i < Col; i++) {
+        printf("행 %d : ", i);
+        for (j = 0; j < Row; j++) {
+            printf("%3d ", S[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
