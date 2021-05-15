53bfdeb91f053082a36722d5c3847da4d4c93e05
rlarjsdn2313 <informationdoor04@gmail.com>
2021-05-15 13:44:57 +0900

    Finish t2(세 사람의 키의 최댓값을 구하는 프로그램

---
+++ 4th Programming 0515/t2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int people = 0;
+    printf("사람 수 : ");
+    scanf("%d", &people);
+
+    int* height = (int*)calloc(people, sizeof(int));
+    for (int i=0;i<people;i++) {
+        printf("height[%d] : ", i);
+        scanf("%d", &height[i]);
+    }
+
+    printf("%d 사람의 키를 입력하세요.\n");
+    
+    int a = 0;
+    for (int i=0;i<people;i++) {
+        if (height[i] >= a) {
+            a = height[i];
+        }
+    }
+
+    printf("최댓값은 %d 입니다.\n", a);
+}
